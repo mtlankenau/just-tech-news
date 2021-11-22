07c0552070d2871b32060bfee7f071b6d93be6b1
mtlankenau <mtlankenau@gmail.com>
2021-11-22 15:36:57 -0500

    created vote table, implemented many to many relationship with user & post, and able to return total vote count per post

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const sequelize = require('../../config/connection');
+const { Post, User, Vote } = require('../../models');
 
 // GET /api/posts
 router.get('/', (req, res) => {
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     order: [['created_at', 'DESC']],
     include: [
       {
@@ -26,7 +33,13 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
       {
         model: User,
@@ -58,6 +71,16 @@ router.post('/', (req, res) => {
     console.log(err);
     res.status(500).json(err);
   });
+});
+
+// PUT /api/posts/upvote  // Creates a vote
+router.put('/upvote', (req, res) => {
+  Post.upvote(req.body, { Vote })
+    .then(updatedPostData => res.json(updatedPostData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 // PUT /api/posts/1
