a70209ec656ded7db0542229f5eeeac1cfc2ada7
mtlankenau <mtlankenau@gmail.com>
2021-11-23 11:20:44 -0500

    finished updating API routes to include the comments model

--- models/index.js
+++ models/index.js
@@ -1,10 +1,27 @@
 const User = require('./User');
 const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 User.hasMany(Post, {
   foreignKey: 'user_id'
 });
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id'
+});
+
+Comment.belongsTo(User, {
+  foreignKey: 'user_id'
+})
 
 Post.belongsTo(User, {
   foreignKey: 'user_id'
@@ -38,4 +55,4 @@ Post.hasMany(Vote, {
   foreignKey: 'post_id'
 });
 
-module.exports = { User, Post, Vote };
+module.exports = { User, Post, Vote, Comment };
