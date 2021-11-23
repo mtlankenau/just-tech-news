6330ff990db9b57288ca43346cd05d2fdd45d582
mtlankenau <mtlankenau@gmail.com>
2021-11-23 11:29:06 -0500

    connect to jawsdb

--- config/connection.js
+++ config/connection.js
@@ -3,11 +3,18 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create the connection to our database, pass in MYSQL information for username & password
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-  host: 'localhost',
-  dialect: 'mysql',
-  port: 3306
-});
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  // create the connection to remote Heroku database
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  // create the connection to our local database, pass in MYSQL information for username & password
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+  });
+}
 
 module.exports = sequelize;
